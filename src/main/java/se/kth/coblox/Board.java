260c50becf7d628bddb8fbc63e59ae1c901b719c
Olivers mac <olieri@kth.se>
2017-10-18 13:15:31 +0200

    I created the constructor and named the most essential methods. I am not finished with the class but Mr.Brandefelt forced me to commit/push.

--- src/main/java/se/kth/coblox/Board.java
+++ src/main/java/se/kth/coblox/Board.java
@@ -1,4 +1,37 @@
 package se.kth.coblox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private Color[][] groundedBlocks;
+    private List<Block> fallingPiece;
+
+    public Board() {
+        groundedBlocks = new Color[5][15];
+        fallingPiece = new ArrayList<>();
+    }
+
+    public void generateNewPiece() {
+        fallingPiece.add(new Block((int) Math.ceil(columns() / 2), rows() - 1, randomColor()));
+    }
+
+    private Color randomColor() {
+        return Color.BLACK;
+    }
+
+    public int RemoveCompleteRows() {
+        return 0;
+    }
+
+    public void performTick() {
+    }
+
+    public int rows() {
+        return groundedBlocks[0].length;
+    }
+
+    public int columns() {
+        return groundedBlocks.length;
+    }
 }
