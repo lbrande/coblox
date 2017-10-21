5d2fbd7ae1844201259ec35b9573d9f4c5a3531b
lbrande <lovbra00@gmail.com>
2017-10-21 12:40:44 +0200

    Centered game in window.

--- gui/src/main/java/se/kth/coblox/gui/GameCanvas.java
+++ gui/src/main/java/se/kth/coblox/gui/GameCanvas.java
@@ -8,6 +8,7 @@ import se.kth.coblox.Game;
 
 public class GameCanvas extends Canvas {
   private static final Color BACKGROUND_COLOR = Color.BLACK;
+  private static final Color BORDER_COLOR = Color.WHITE;
   private static HashMap<se.kth.coblox.Color, Color> blockColors;
 
   public GameCanvas() {
@@ -21,10 +22,10 @@ public class GameCanvas extends Canvas {
 
   private void populateBlockColors() {
     blockColors = new HashMap<>();
-    blockColors.put(se.kth.coblox.Color.BLACK, Color.DARKGRAY);
-    blockColors.put(se.kth.coblox.Color.BLUE, Color.BLUE);
+    blockColors.put(se.kth.coblox.Color.BLACK, Color.DIMGRAY);
+    blockColors.put(se.kth.coblox.Color.BLUE, Color.DARKBLUE);
     blockColors.put(se.kth.coblox.Color.GREEN, Color.DARKGREEN);
-    blockColors.put(se.kth.coblox.Color.ORANGE, Color.ORANGE);
+    blockColors.put(se.kth.coblox.Color.ORANGE, Color.DARKORANGE);
     blockColors.put(se.kth.coblox.Color.PURPLE, Color.PURPLE);
     blockColors.put(se.kth.coblox.Color.RED, Color.DARKRED);
     blockColors.put(se.kth.coblox.Color.WHITE, Color.WHITE);
@@ -35,15 +36,21 @@ public class GameCanvas extends Canvas {
     graphicsContext.setFill(BACKGROUND_COLOR);
     graphicsContext.fillRect(0, 0, getWidth(), getHeight());
     double squareSize = getHeight() / game.getBoard().rows();
+    double startX = (getWidth() - game.getBoard().columns() * squareSize) / 2;
     for (int row = 0; row < game.getBoard().rows(); row++) {
       for (int column = 0; column < game.getBoard().columns(); column++) {
         se.kth.coblox.Color blockColor = game.getBoard().getBlockColor(column, row);
         if (blockColor != null) {
           graphicsContext.setFill(blockColors.get(blockColor));
           graphicsContext.fillRect(
-              column * squareSize, getHeight() - (row + 1) * squareSize, squareSize, squareSize);
+              startX + column * squareSize,
+              getHeight() - (row + 1) * squareSize,
+              squareSize,
+              squareSize);
         }
       }
     }
+    graphicsContext.setStroke(BORDER_COLOR);
+    graphicsContext.strokeRect(startX, 0, game.getBoard().columns() * squareSize, getHeight());
   }
 }
